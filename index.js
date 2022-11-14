6da209aeb4e6e104244ef773355917d8e73e8e3a
muddada.rao <muddada.rao@deltatech.gg>
2022-11-14 16:16:20 +0530

    merging pdf completed

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var fs = require('fs');
 var pdf = require('dynamic-html-pdf');
 var html = fs.readFileSync('template.html', 'utf8');
 
+const PDFMerger = require('pdf-merger-js');
+
+var merger = new PDFMerger();
+
 pdf.registerHelper('ifCond', function (v1, v2, options) {
     if (v1 === v2) {
         return options.fn(this);
@@ -51,3 +55,20 @@ for(var i=0;i<users.length; i++){
             console.error(error)
         });
 }
+
+(async () => {
+    await merger.add('output0.pdf');
+    await merger.add('output1.pdf');
+    await merger.add('output2.pdf');  //merge all pages. parameter is the path to file and filename.
+    // await merger.add('pdf2.pdf', 2); // merge only page 2
+    // await merger.add('pdf2.pdf', [1, 3]); // merge the pages 1 and 3
+    // await merger.add('pdf2.pdf', '4, 7, 8'); // merge the pages 4, 7 and 8
+    // await merger.add('pdf3.pdf', '3 to 5'); //merge pages 3 to 5 (3,4,5)
+    // await merger.add('pdf3.pdf', '3-5'); //merge pages 3 to 5 (3,4,5)
+  
+    await merger.save('merged.pdf'); //save under given name and reset the internal document
+    
+    // Export the merged PDF as a nodejs Buffer
+    // const mergedPdfBuffer = await merger.saveAsBuffer();
+    // fs.writeSync('merged.pdf', mergedPdfBuffer);
+  })();

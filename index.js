e408462d213a06a630c36c6882ab75a08179d93d
muddada.rao <muddada.rao@deltatech.gg>
2022-11-14 16:48:04 +0530

    dynamically generated data

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ var users = [
     }
 ];
 
+let pdfPromises = [];
+
 for(var i=0;i<users.length; i++){
     var document = {
         type: 'file',     // 'file' or 'buffer'
@@ -47,28 +49,14 @@ for(var i=0;i<users.length; i++){
         path: `./output${i}.pdf`    // it is not required if type is buffer
     };
     
-    pdf.create(document, options)
-        .then(res => {
-            console.log(res)
-        })
-        .catch(error => {
-            console.error(error)
-        });
+    pdfPromises[i] = pdf.create(document, options);
 }
+let mergedPromises = [];
+Promise.all(pdfPromises).then(async(result) => {
+    for(let i = 0; i < result.length; i++) {
+        mergedPromises[i] = await merger.add(`output${i}.pdf`);
+    }
+    merger.save('merged.pdf'); //save under given name and reset the internal document
 
-(async () => {
-    await merger.add('output0.pdf');
-    await merger.add('output1.pdf');
-    await merger.add('output2.pdf');  //merge all pages. parameter is the path to file and filename.
-    // await merger.add('pdf2.pdf', 2); // merge only page 2
-    // await merger.add('pdf2.pdf', [1, 3]); // merge the pages 1 and 3
-    // await merger.add('pdf2.pdf', '4, 7, 8'); // merge the pages 4, 7 and 8
-    // await merger.add('pdf3.pdf', '3 to 5'); //merge pages 3 to 5 (3,4,5)
-    // await merger.add('pdf3.pdf', '3-5'); //merge pages 3 to 5 (3,4,5)
-  
-    await merger.save('merged.pdf'); //save under given name and reset the internal document
-    
-    // Export the merged PDF as a nodejs Buffer
-    // const mergedPdfBuffer = await merger.saveAsBuffer();
-    // fs.writeSync('merged.pdf', mergedPdfBuffer);
-  })();
+});
+
